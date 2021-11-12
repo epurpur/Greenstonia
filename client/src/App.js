e8ef737ca800a2cb19859adc54a319030c7a6575
epurpur <epurpur@gmail.com>
2021-11-12 12:19:32 -0500

    blank react app in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,24 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-/* CSS styles */
-import "./App.css";
-
-/* Pages */
-import Homepage from "./pages/Homepage";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import LandingPage from "./pages/LandingPage";
-import NewJob from "./pages/NewJob";
-import SingleJobPage from "./pages/SingleJobPage";
-
-/* Components */
-import Header from "./components/Header";
-import ContractorRegistrationForm from "./components/ContractorRegistrationForm";
-import WorkerRegistrationForm from "./components/WorkerRegistrationForm";
-
-/* User Context Provider */
-import UserProvider from "./utils/UserContext";
-
-/* Apollo Setup */
-const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <UserProvider>
-        <Router>
-          <div className="App">
-            <Header />
-
-            {/* Establishing routes to all endpoints */}
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/About">
-                <About />
-              </Route>
-              <Route exact path="/Login">
-                <Login />
-              </Route>
-              <Route exact path="/ContractorRegistration">
-                <ContractorRegistrationForm />
-              </Route>
-              <Route exact path="/WorkerRegistration">
-                <WorkerRegistrationForm />
-              </Route>
-              <Route exact path="/LandingPage">
-                <LandingPage />
-              </Route>
-              <Route exact path="/NewJob">
-                <NewJob />
-              </Route>
-
-              {/* This URL will eventually be changed to /SingleJobPage/:id */}
-              <Route exact path="/SingleJobPage/:id">
-                <SingleJobPage />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </UserProvider>
-    </ApolloProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
