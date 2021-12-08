b153f69820e634d8226e93d3f8138e21f8bd519a
epurpur <epurpur@gmail.com>
2021-12-08 11:37:55 -0500

    styling most basic homepage layout

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -10,10 +10,18 @@ const HomePage = () => {
   
     return (
         <>
-        <Header />
-        <section>
-        Home Page
-        </section>
+            <Header />
+            <section>
+                {/* These items will all become individual components */}
+                {/* Don't forget to change CSS for them too */}
+                <div id='alerts'>Alerts</div>
+                <div id='search'>Search </div>
+                <div id='routeFinders'>
+                    <div id='map'>Search by map</div>
+                    <div id='routeList'>Search by name</div>
+                </div>
+                <div id='weather'>Weather</div>
+            </section>
         </>
     );
   };
