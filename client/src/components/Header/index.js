29d86b5c8681981a67cae0c5f50a9e006e6fdd36
epurpur <epurpur@gmail.com>
2021-12-08 15:37:58 -0500

    changing header font

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,17 +12,17 @@ const Header = () => {
       <>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-          <Navbar.Brand href="#home" id='logo'>Greenstonia</Navbar.Brand>
+          <Navbar.Brand href="/home" id='logo'>Greenstonia</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Weather</Nav.Link>
-              <Nav.Link href="#pricing">Areas</Nav.Link>
-              <Nav.Link href="#history">History</Nav.Link>
-              <Nav.Link href="#Contact">Contact</Nav.Link>
+              <Nav.Link href="/weather">Weather</Nav.Link>
+              <Nav.Link href="/areas">Areas</Nav.Link>
+              <Nav.Link href="/history">History</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">Login</Nav.Link>
+              <Nav.Link>Login</Nav.Link>
             </Nav>
           </Navbar.Collapse>
           </Container>
