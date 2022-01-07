471f0a021669247e489a66e4fb60ced14c5e96c5
epurpur <epurpur@gmail.com>
2022-01-07 17:50:10 -0500

    figuring out api call for weather data

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 /* Components */
 import Header from "../../components/Header";
@@ -14,7 +14,41 @@ import WeatherComponent from "../../components/WeatherComponent";
 import "./styles.css";
 
 const HomePage = () => {
-  
+    
+    const [weatherData, setweatherData] = useState(null);
+
+    // Getting weather info in order to pass as props to Conditions Alert and Weather Component
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=charlottesville&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log('DATA:',data);
+                // setweatherData(data);
+            })
+    }, []);
+    
+
+
+
+
+
+
+    // make API request for today's weather forecast in requested city
+    // fetch(url)
+    //     .then(function (response) {
+    //         if (response.ok) {             //if user input is a valid city name
+    //             return response.json(); 
+    //         } else {
+    //             alert('bad response');
+    //             return Promise.reject(response)
+    //         }
+    //     })
+    //     .then(function (data) {
+    //         console.log('Boone data: ', data);
+    //     });
+
     return (
         <>
             <Header />
