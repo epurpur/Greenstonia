72ee23b7859a6df25f7dcfec5e8917c102922522
epurpur <epurpur@gmail.com>
2022-01-14 16:27:56 -0500

    working on getting correct value for UTC timestamp for yesterday and two days ago

--- client/src/components/WeatherComponent/index.js
+++ client/src/components/WeatherComponent/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import timespan from 'jsonwebtoken/lib/timespan';
+import React, { useState, useEffect } from 'react';
 
 /* Components */
 
 const WeatherComponent = () => {
+
+    const [historicalWeatherData, sethistoricalWeatherData] = useState(null);
+
+    useEffect(() => {
+
+        // need to get UTC time for each of last two days
+        // START HERE. Figure out how to get correct UTC timestamp
+        const currentTimestamp = new Date().getTime();
+        const yesterdayTimestamp = currentTimestamp - (24*60*60*1000);
+        const twoDaysAgoTimestamp = currentTimestamp - 24*60*60*1000*2;
+
+        console.log(yesterdayTimestamp, twoDaysAgoTimestamp);
+    
+        // API call to OpenWeatherMap for previous two days weather
+        fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.8849&lon=78.8995&dt=${yesterdayTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log('HISTORICAL WEATHER DATA: ', data);
+            })
+    })
+
     return (
         <>
             <div id='weather'>Weather</div>
--- client/src/pages/WeatherPage/index.js
+++ client/src/pages/WeatherPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 /* Components */
 import Header from "../../components/Header";
