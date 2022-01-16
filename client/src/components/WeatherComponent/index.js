375391edb0636874b4cb1c139c95ae0af1758723
epurpur <epurpur@gmail.com>
2022-01-15 21:57:33 -0500

    packaging daily weather stats for historical weather forecast

--- client/src/components/WeatherComponent/index.js
+++ client/src/components/WeatherComponent/index.js
@@ -7,6 +7,39 @@ const WeatherComponent = () => {
     const [yesterdayWeather, setyesterdayWeather] = useState(null);
     const [twoDaysAgoWeather, settwoDaysAgoWeather] = useState(null);
 
+    const parseHistoricalDailyWeather = (dailyWeatherData) => {
+        // takes daily weather data from API call and returns array of weather data for that day
+        // this is for historical daily weather (ie: yesterday and two days ago weather)
+
+        // remember, there is a 24 hour hourly forecast. I am just taking weather at noon of requested day
+        const dailyTemps = [];
+        const dailyHumidity = [];
+        const dailyWind = [];
+        const dailyClouds = [];   // this is measured as a percentage (ie: 50% cloudy)
+
+        for (const hourlyWeather of dailyWeatherData['hourly']) {
+            // add hourly measurements reading to dailyTemps
+            dailyTemps.push(hourlyWeather['temp']);
+            dailyHumidity.push(hourlyWeather['humidity']);
+            dailyWind.push(hourlyWeather['wind_speed']);
+            dailyClouds.push(hourlyWeather['clouds']);
+        }
+        const hiTemp = Math.max(...dailyTemps).toFixed(2);  //toFixed = round to 2 decimal places
+        const loTemp = Math.min(...dailyTemps).toFixed(2);
+
+        // must write function to get avg of array
+        const getAverage = (array) => array.reduce((a,b) => a + b) / array.length
+        const avgHumidity = getAverage(dailyHumidity).toFixed(2);
+        const avgWind = getAverage(dailyWind).toFixed(2);
+        const avgClouds = getAverage(dailyClouds).toFixed(2);
+        const precip = 'n/a';
+
+        // THIS IS THE STRUCTURE OF THE DATA RETURNED FROM API CALL
+        const dailyWeatherStats = [hiTemp, loTemp, avgHumidity, avgWind, avgClouds, precip]
+        
+        return dailyWeatherStats;
+    }
+
     useEffect(() => {
 
         // need to get UTC time for each of last two days
@@ -26,38 +59,18 @@ const WeatherComponent = () => {
                 return response.json();
             })
             .then((data) => {
-                // remember, there is a 24 hour hourly forecast. I am just taking weather at noon of requested day
-                const dailyTemps = [];
-                const dailyHumidity = [];
-                const dailyWind = [];
-
-                for (const hourlyWeather of data['hourly']) {
-                    // add hourly measurements reading to dailyTemps
-                    dailyTemps.push(hourlyWeather['temp']);
-                    dailyHumidity.push(hourlyWeather['humidity']);
-                    dailyWind.push(hourlyWeather['wind_speed']);
-                }
-                const hiTemp = Math.max(...dailyTemps).toFixed(2);  //toFixed = round to 2 decimal places
-                const loTemp = Math.min(...dailyTemps).toFixed(2);
-                
-                // must write function to get avg humidity
-                const getAverage = (array) => array.reduce((a,b) => a + b) / array.length
-                const avgHumidity = getAverage(dailyHumidity).toFixed(2);
-                const avgWind = getAverage(dailyWind).toFixed(2);
-
-
-                console.log(hiTemp, loTemp, avgHumidity, avgWind);
+                parseHistoricalDailyWeather(data);
             });
 
         // API call to OpenWeatherMap for two days ago weather
-        // fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=38.0685&lon=-78.8895&dt=${twoDaysAgoTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
-        //     .then(response => {
-        //         return response.json();
-        //     })
-        //     .then((data) => {
-        //         // remember, there is a 24 hour hourly forecast. I am just taking weather at noon of requested day
-        //         console.log('TWO DAYS AGO WEATHER DATA: ', data);
-        //     });
+        fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.8849&lon=-78.8995&dt=${twoDaysAgoTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                // remember, there is a 24 hour hourly forecast. I am just taking weather at noon of requested day
+                parseHistoricalDailyWeather(data);
+            });
 
 
     })
