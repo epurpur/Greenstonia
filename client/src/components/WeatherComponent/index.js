7f1160923688408f4f71b0dd2c1f81956f0f1810
epurpur <epurpur@gmail.com>
2022-01-16 20:38:43 -0500

    temporarily turning off weather API calls

--- client/src/components/WeatherComponent/index.js
+++ client/src/components/WeatherComponent/index.js
@@ -10,16 +10,6 @@ const WeatherComponent = () => {
     // state for historical weather
     const [yesterdayWeather, setyesterdayWeather] = useState(null);
     const [twoDaysAgoWeather, settwoDaysAgoWeather] = useState(null);    
-    
-    // state for dates
-    const [min2days, setmin2days] = useState(null); 
-    const [yesterday, setyesterday] = useState(null);
-    const [today, settoday] = useState(null);
-    const [tomorrow, settomorrow] = useState(null);
-    const [pl2days, setpl2days] = useState(null);
-    const [pl3days, setpl3days] = useState(null);
-    const [pl4days, setpl4days] = useState(null);
-
 
     const parseHistoricalDailyWeather = (dailyWeatherData) => {
         // takes daily weather data from API call and returns array of weather data for that day
@@ -49,45 +39,54 @@ const WeatherComponent = () => {
         const precip = 'n/a';
 
         // THIS IS THE STRUCTURE OF THE DATA RETURNED FROM API CALL
-        const dailyWeatherStats = [hiTemp, loTemp, avgHumidity, avgWind, avgClouds, precip]
+        const dailyWeatherStats = {
+            "hiTemp":hiTemp, 
+            "loTemp":loTemp, 
+            "humidity":avgHumidity, 
+            "wind":avgWind, 
+            "clouds":avgClouds, 
+            "precip":precip
+        }
         
-        return dailyWeatherStats;
+        setyesterdayWeather(dailyWeatherStats);
     }
 
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     // need to get UTC time for each of last two days
-    //     // START HERE. Figure out how to get correct UTC timestamp
-    //     const currentTimestamp = new Date().getTime();
-    //     console.log('currentTimestamp: ', currentTimestamp - (24*60*60*1000));
-    //     let yesterdayTimestamp = currentTimestamp - (24*60*60*1000)
-    //     let twoDaysAgoTimestamp = currentTimestamp - (24*60*60*1000*2);
+        // need to get UTC time for each of last two days
+        // START HERE. Figure out how to get correct UTC timestamp
+        const currentTimestamp = new Date().getTime();
+        console.log('currentTimestamp: ', currentTimestamp - (24*60*60*1000));
+        let yesterdayTimestamp = currentTimestamp - (24*60*60*1000)
+        let twoDaysAgoTimestamp = currentTimestamp - (24*60*60*1000*2);
 
-    //     //need to remove last 3 digits of UTC timestamp and convert to integer
-    //     yesterdayTimestamp = Math.floor(yesterdayTimestamp/1000);
-    //     twoDaysAgoTimestamp = Math.floor(twoDaysAgoTimestamp/1000);
+        //need to remove last 3 digits of UTC timestamp and convert to integer
+        yesterdayTimestamp = Math.floor(yesterdayTimestamp/1000);
+        twoDaysAgoTimestamp = Math.floor(twoDaysAgoTimestamp/1000);
     
-    //     // API call to OpenWeatherMap for yesterday's weather
-    //     fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.8849&lon=-78.8995&dt=${yesterdayTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
-    //         .then(response => {
-    //             return response.json();
-    //         })
-    //         .then((data) => {
-    //             parseHistoricalDailyWeather(data);
-    //         });
+        // // API call to OpenWeatherMap for yesterday's weather
+        // fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.8849&lon=-78.8995&dt=${yesterdayTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
+        //     .then(response => {
+        //         return response.json();
+        //     })
+        //     .then((data) => {
+        //         parseHistoricalDailyWeather(data);
+        //     });
 
-    //     // API call to OpenWeatherMap for two days ago weather
-    //     fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.8849&lon=-78.8995&dt=${twoDaysAgoTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
-    //         .then(response => {
-    //             return response.json();
-    //         })
-    //         .then((data) => {
-    //             // remember, there is a 24 hour hourly forecast. I am just taking weather at noon of requested day
-    //             parseHistoricalDailyWeather(data);
-    //         });
+        // // API call to OpenWeatherMap for two days ago weather
+        // fetch(`https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=37.8849&lon=-78.8995&dt=${twoDaysAgoTimestamp}&appid=333de4e909a5ffe9bfa46f0f89cad105&units=imperial`)
+        //     .then(response => {
+        //         return response.json();
+        //     })
+        //     .then((data) => {
+        //         // remember, there is a 24 hour hourly forecast. I am just taking weather at noon of requested day
+        //         parseHistoricalDailyWeather(data);
+        //     });
 
 
-    // })
+    });
+
+    console.log('Yesterday Weather', yesterdayWeather);
 
     ///////////////////////////////////////
     // get dates for injecting into HTML //
@@ -96,40 +95,30 @@ const WeatherComponent = () => {
     let minus2days = new Date();
     minus2days.setDate(minus2days.getDate() -2);
     minus2days = minus2days.toLocaleDateString();
-    useEffect(() => {setmin2days(minus2days)});
 
-    // ayer means yesterday in spanish
-    let ayer = new Date();
-    ayer.setDate(ayer.getDate() - 1);
-    ayer = ayer.toLocaleDateString();
-    useEffect(() => {setyesterday(ayer)});
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday = yesterday.toLocaleDateString();
     
-    // hoy mean today in spanish
-    let hoy = new Date();
-    hoy.setDate(hoy.getDate());
-    hoy = hoy.toLocaleDateString();
-    useEffect(() => {settoday(hoy)});
+    let today = new Date();
+    today.setDate(today.getDate());
+    today = today.toLocaleDateString();
 
-    // manana means tomorrow in spanish
-    let manana = new Date();
-    manana.setDate(manana.getDate() + 1);
-    manana = manana.toLocaleDateString();
-    useEffect(() => {settomorrow(manana)});
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow = tomorrow.toLocaleDateString();
 
     let plus2days = new Date();
     plus2days.setDate(plus2days.getDate() + 2);
     plus2days = plus2days.toLocaleDateString();
-    useEffect(() => {setpl2days(plus2days)});
 
     let plus3days = new Date();
     plus3days.setDate(plus3days.getDate() + 3);
     plus3days = plus3days.toLocaleDateString();
-    useEffect(() => {setpl3days(plus3days)});
 
     let plus4days = new Date();
     plus4days.setDate(plus4days.getDate() + 4);
     plus4days = plus4days.toLocaleDateString();
-    useEffect(() => {setpl4days(plus4days)});
 
     return (
         <>
@@ -138,7 +127,7 @@ const WeatherComponent = () => {
                 <div id='dailyBoxes'>
                     <div id='dailyBox'>
                         <p id="day">-2 Days</p>
-                        <p id="date">{min2days}</p>
+                        <p id="date">{minus2days}</p>
                         <p>Hi Temp:</p>
                         <p>Lo Temp: </p>
                         <p>Precip: </p>
@@ -174,7 +163,7 @@ const WeatherComponent = () => {
                     </div>
                     <div id='dailyBox'>
                         <p id="day">+2 Days</p>
-                        <p id="date">{pl2days}</p>
+                        <p id="date">{plus2days}</p>
                         <p>Hi Temp: </p>
                         <p>Lo Temp: </p>
                         <p>Precip: </p>
@@ -183,7 +172,7 @@ const WeatherComponent = () => {
                     </div>
                     <div id='dailyBox'>
                         <p id="day">+3 Days</p>
-                        <p id="date">{pl3days}</p>
+                        <p id="date">{plus3days}</p>
                         <p>Hi Temp: </p>
                         <p>Lo Temp: </p>
                         <p>Precip: </p>
@@ -192,7 +181,7 @@ const WeatherComponent = () => {
                     </div>
                     <div id='dailyBox'>
                         <p id="day">+4 Days</p>
-                        <p id="date">{pl4days}</p>
+                        <p id="date">{plus4days}</p>
                         <p>Hi Temp: </p>
                         <p>Lo Temp: </p>
                         <p>Precip: </p>
