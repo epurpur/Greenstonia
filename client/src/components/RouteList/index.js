583b22e3db942e12791c613888331f5292d00ca3
epurpur <epurpur@gmail.com>
2022-02-01 09:24:00 -0500

    displaying dynamic areas cards in routeList component

--- client/src/components/RouteList/index.js
+++ client/src/components/RouteList/index.js
@@ -7,22 +7,27 @@ import { QUERY_AREAS } from '../../utils/queries';
 import "./styles.css";
 
 const RouteList = (props) => {
-    console.log('From routelist component',props)
+    
+    let climbingAreas = props.areas
+    console.log(climbingAreas)
+    // sort climbing areas alphabetically by name
+    // need to make copy of climbingAreas array to do this
+    const arrayForSort = [...climbingAreas]
+    const climbingAreasByName = arrayForSort.sort((a, b) => a.areaName < b.areaName ? -1 : (a.areaName > b.areaName ? 1 : 0))
+
     return (
         <>
             <section id='routeList'>
                 <div id='routeListTitle'>Search Areas by Name (RouteList component)</div>
                 <div id='areaCards'>
-                    {/* This will be replaced with an API call for all areas */}
-                    <p>Rock Point Overlook</p>
-                    <p>The Forest</p>
-                    <p>Drive By (Mile 5)</p>
-                    <p>12.2</p>
-                    <p>Snail Boulder</p>
+                    {climbingAreasByName.map((area) =>
+                    // create card for each climbing area. These are sorted into alphabetical order
+                        (
+                            <p className='areaCard'>{area.areaName}</p>
+                        )
+                    )}
                 </div>
             </section>
-            
-
         </>
     )
 };
