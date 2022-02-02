0ad2715853a0b6149cc7ffa92fc7dceee003f453
epurpur <epurpur@gmail.com>
2022-02-02 09:41:11 -0500

    adding map to areaMap page

--- client/src/pages/AreasPage/index.js
+++ client/src/pages/AreasPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 /* Components */
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 /* CSS styles */
 import "./styles.css";
@@ -24,9 +26,19 @@ const AreasPage = () => {
                     <p>Area Name: {areaData.areaName} </p>
                     <p>Area Description: {areaData.areaDescription} </p>
                     <p>Parking Description: {areaData.parkingDescription} </p>
+                    <p>List of boulders in this area go here</p>
                 </div>
-                <div>
-                    Map goes here
+                <div id="boulderMap">
+                    <MapContainer center={[37.95, -78.98]} zoom={11.25} scrollWheelZoom={false}>
+                        {/* Google Maps basemap as TileLayer */}
+                        <TileLayer url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"/>
+                            <Marker position={[37.95, -78.93]}>
+                                <Popup>
+                                    <h2>Mustang Boulder</h2>
+                                    <Link to="/boulder">Click to view area</Link>
+                                </Popup>
+                            </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Footer />
