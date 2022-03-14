aa1857a8eec89889dad9c963b972fc4861779f40
Erich Purpur <epurpur@gmail.com>
2022-03-14 15:33:53 -0400

    passing route data into singleRoute Page

--- client/src/pages/SingleRoutePage/index.js
+++ client/src/pages/SingleRoutePage/index.js
@@ -15,16 +15,17 @@ import star_img from "../../images/star.png";
 const SingleRoutePage = () => {
 
     let singleRouteData = useLocation();
+    singleRouteData = singleRouteData.state;
     console.log('SINGLE ROUTE DATA', singleRouteData);
 
     return (
         <>
             <Header />
-            <h2> SuperCharger </h2>
-            <div> Grade: v7</div>
-            <div> Quality: 3 stars </div>
-            <div> First Ascent: Jacob Schierman </div>
-            <div> Description: blah blah blah </div>
+            <h2> {singleRouteData.routeName} </h2>
+            <div> Grade: {singleRouteData.routeGrade} </div>
+            <div> Quality: {singleRouteData.routeQuality} stars </div>
+            <div> First Ascent: {singleRouteData.firstAscent} </div>
+            <div> Description: {singleRouteData.routeDescription} </div>
             <div id="singleRoutePhoto"> Photo goes here </div>
             <div id="singleRouteVideo"> Video goes here </div>
             <Footer />
