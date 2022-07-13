4caacce47a7af2d7d57a8f9b8e48c22d48d40562
Erich Purpur <epurpur@gmail.com>
2022-07-13 09:29:59 -0400

    still struggling with timer

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 
 /* Components */ 
 import { Navbar, Container, Nav } from 'react-bootstrap';
@@ -11,11 +11,33 @@ import { UserContext } from "../../utils/UserContext";
 
 const Header = () => {
   
-    const {login, setLogin} = useContext(UserContext);
+    const { login, setLogin } = useContext(UserContext);
+    const [ seconds, setSeconds ] = useState(5);
+    
+  
 
     const loginUser = (event) => {
       //using ternary operator
+      //login of 1 = user is logged in, else user is not logged in.
       login === 0 ? setLogin(1) : setLogin(0);
+
+      //timer function
+      // if (seconds > 0) {
+      //   const interval = setInterval(() => {
+      //     setSeconds((seconds) => seconds - 1)
+      //   }, 1000);
+      // } else {
+      //   clearInterval()
+      // }
+
+      //new timer functio
+      const interval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds((seconds) => seconds - 1)
+        } else {
+          clearInterval(interval)
+        }
+      }, 1000);
     }
 
     return (
@@ -32,7 +54,7 @@ const Header = () => {
             </Nav>
             <Nav>
               <Nav.Link>Login</Nav.Link>
-              <button onClick={loginUser}>{login}</button>
+              <button id='counter' onClick={loginUser}>{login} Seconds: {seconds} </button>
             </Nav>
           </Navbar.Collapse>
           </Container>
@@ -41,4 +63,4 @@ const Header = () => {
     );
   };
   
-  export default Header;
+export default Header;
