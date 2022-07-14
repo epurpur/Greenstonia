0fa5d02a8930c62d1474959e31bd49477c01bb3d
Erich Purpur <epurpur@gmail.com>
2022-07-14 14:25:21 -0400

    fixing manifest error

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -4,7 +4,10 @@ export const UserContext = React.createContext();
 
 
 const UserProvider = (props) => {
-  const [login, setLogin] = useState(0);
+  const [login, setLogin] = useState({
+    //setting initial value of login to 0
+    login: 0
+  });
 
   return (
     //passing in values from login state to use elsewhere in app
