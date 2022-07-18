44b9edfd1bbd880ce33b11560047c64f04d516bd
Erich Purpur <epurpur@gmail.com>
2022-07-18 10:51:00 -0400

    building login modal

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, } from "react";
 
 /* Components */ 
-import { Navbar, Container, Nav, Alert } from 'react-bootstrap';
+import { Navbar, Container, Nav, Alert, Modal, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import LoginModal from "../LoginModal";
 
@@ -19,14 +19,28 @@ const Header = () => {
     const [loginAlert, setLoginAlert] = useState(0);
     const [logoutAlert, setLogoutAlert] = useState(0);
     //sets state of Modal for login button
-    const [modal, setModal] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+    const handleCloseModal = () => setShowModal(false);
+    const handleShowModal = () => setShowModal(true);
+    //handles input in Login modal
+    const [ formState, setFormState ] = useState({ username: '', password: ''})
+    const handleChange = (event) => {
+      const {name, value} = event.target;
+      setFormState({
+        ...formState,
+        [name]: value
+      })
+      console.log('name:', name, 'value:', value)
+    }
+
+
 
     const handleUserLogin = (event) => {
       //login of 1 = user is logged in, else user is not logged in.
       if (login === 0) {
         setLogin(1)
-        setModal(1)  // creates modal popup for login
         makeAlert()
+        handleCloseModal() // remove modal
       } else {
         setLogin(0)
         makeAlert()
@@ -91,7 +105,7 @@ const Header = () => {
             </Nav>
             <Nav>
               {login === 0 ? 
-                <Nav.Link onClick={handleUserLogin}>Login</Nav.Link> 
+                <Nav.Link onClick={handleShowModal}>Login</Nav.Link> 
                 : 
                 <Nav.Link onClick={handleUserLogin}>Logout</Nav.Link>
               }
@@ -100,8 +114,36 @@ const Header = () => {
           </Navbar.Collapse>
           </Container>
         </Navbar> 
-        {/* render modal on clicking 'login' */}
-        {modal === 1 ? <LoginModal /> : null}
+        <Modal id='loginModalForm' show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+            <Form>
+              <Form.Group className="m-3">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control 
+                    name="email"
+                    value={formState.email || ''}
+                    onChange={handleChange}
+                    required
+                />
+              </Form.Group>
+              <Form.Group className="m-3">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control 
+                    name="password"
+                    value={formState.password || ''}
+                    onChange={handleChange}
+                    required
+                />
+              </Form.Group>
+            </Form>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
+            {/* <Button variant="primary" onClick={handleUserLogin}>Login</Button> */}
+            <Button className="btnFormSend" variat="outline-success" onClick={handleUserLogin}>Login</Button>
+          </Modal.Footer>
+        </Modal>
         {/* render login/logout alert */}
         {loginAlert === 1 ?
           <Alert className='alertText' variant='info'>You are now logged in</Alert> : null
