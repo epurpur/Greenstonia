ff50dfbb56b3ba190c8cf10e5c79ff2b2a5e0313
Erich Purpur <epurpur@gmail.com>
2022-07-29 15:46:13 -0400

    adding embedded youtube videos into single route page

--- client/src/pages/SingleRoutePage/index.js
+++ client/src/pages/SingleRoutePage/index.js
@@ -4,13 +4,10 @@ import { Link, useLocation } from "react-router-dom";
 /* Components */
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import ImageCards from '../../components/ImageCards';
 
 /* CSS styles */
 import "./styles.css";
-
-/* Images */
-import star_img from "../../images/star.png";
-
 
 const SingleRoutePage = () => {
 
@@ -24,12 +21,26 @@ const SingleRoutePage = () => {
                 <div id='routeInfoHolder'>
                     <p id="breadcrumb">Go Back: <Link to={{pathname: `/boulder/${singleRouteData.boulderName}`, state: {key: singleRouteData.boulderID, boulderID: singleRouteData.boulderID, boulderName: singleRouteData.boulderName, areaName: singleRouteData.areaName }}}>{singleRouteData.boulderName}</Link></p>
                     <h2> {singleRouteData.routeName} </h2>
-                    <div> Grade: {singleRouteData.routeGrade} </div>
-                    <div> Quality: {singleRouteData.routeQuality} stars </div>
-                    <div> First Ascent: {singleRouteData.firstAscent} </div>
-                    <div> Description: {singleRouteData.routeDescription} </div>
-                    <div id="singleRoutePhoto"> Photo goes here </div>
-                    <div id="singleRouteVideo"> Video goes here </div>
+                    <div><span>Grade: </span> {singleRouteData.routeGrade} </div>
+                    <div><span>Quality: </span> {singleRouteData.routeQuality} stars </div>
+                    <div><span>First Ascent: </span>{singleRouteData.firstAscent} </div>
+                    <div><span>Description: </span>{singleRouteData.routeDescription} </div>
+                    <div style={{"text-decoration":"underline", "font-size":"25px"}}> Route Images </div>
+                    <div id='imageCardsBox'>
+                        <ImageCards imageInfo={'placeholder'}/>
+                    </div>
+                    <div style={{"text-decoration":"underline", "font-size":"25px"}}> Route videos </div>
+                    <div id="singleRouteVideo">
+                        <iframe 
+                            width="653" 
+                            height="400" 
+                            src="https://www.youtube.com/embed/pWfdPW5mVBI"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            title={singleRouteData.routeName}
+                        />
+                    </div>
                 </div>
             <Footer />
         </>
