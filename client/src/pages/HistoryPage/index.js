ce00d94cbed994e294bfcc2b2de33d5fea139e8d
Erich Purpur <epurpur@gmail.com>
2022-08-17 10:55:06 -0400

    styling and organizing history cards, finally

--- client/src/pages/HistoryPage/index.js
+++ client/src/pages/HistoryPage/index.js
@@ -13,108 +13,121 @@ const HistoryPage = () => {
     const [historyStory, setHistoryStory] = useState([
         {
             id: 1,
-            title: 'Barry Condron history',
+            title: 'Birth of Greenstone Bouldering',
             active: true,
             info: `this is test info`,
-            imgLink: 'barrycondron'
+            imgLink: 'barrycondron',
+            author: 'Barry Condron'
         },
         {
             id: 2,
-            title: 'Elliot Gaunt 12.2',
+            title: 'Blue Ridge Beauty',
             active: true,
             info: `more test info`,
-            imgLink: 'elliotgaunt'
+            imgLink: 'elliotgaunt',
+            author: 'Elliot Gaunt'
         },
         {
             id: 3,
-            title: 'Brad Ausink 12.2',
+            title: 'A Long Way from Thailand',
             active: true,
             info: 'Brad Ausink story',
-            imgLink: 'bradausink'
+            imgLink: 'bradausink',
+            author: 'Brad Ausink'
         },
         {
             id: 4,
-            title: 'Matt Behrens story',
+            title: "Straight from the horse's mouth",
             active: true,
             info: 'Matt Behrens Story',
-            imgLink: 'mattbehrens'
+            imgLink: 'mattbehrens',
+            author: 'Matt Behrens'
         },
         {
             id: 5,
-            title: 'Matt Fanning story',
+            title: 'The skyline drive rennaisance',
             active: true,
             info: 'Matt Fanning Story',
-            imgLink: 'mattfanning'
+            imgLink: 'mattfanning',
+            author: 'Matt Fanning'
         },
         {
             id: 6,
-            title: 'Dave Cohen FA of Life After Greenstone',
+            title: 'Life After Greenstone',
             active: true,
             info: 'dave cohen story of life after greenstone',
-            imgLink: 'davecohen'
+            imgLink: 'davecohen',
+            author: 'Dave Cohen'
         },
         {
             id: 7,
-            title: 'Andrew Cassidy FA of Mycotic Break',
+            title: 'Mycotic Break',
             active: true,
             info: 'Andrew Cassidy story of Mycotic Break',
-            imgLink: 'andrewcassidy'
+            imgLink: 'andrewcassidy',
+            author: 'Andrew Cassidy'
         },
         {
             id: 8,
             title: 'Parker Smith story',
             active: true,
             info: 'Parker Smith Story',
-            imgLink: 'parkersmith'
+            imgLink: 'parkersmith',
+            author: 'Parker Smith'
         },
         {
             id: 9,
-            title: 'Ross Elliot FA of Super Duper Snake',
+            title: 'Super Duper Snake',
             active: true,
             info: 'Ross Elliot FA of Super Duper Snake',
-            imgLink: 'rosselliot'
+            imgLink: 'rosselliot',
+            author: 'Ross Elliott'
         },
         {
             id: 10,
-            title: 'Erich Purpur more 12.2 FAs',
+            title: 'Filling in the lines',
             active: true,
             info: 'Story of some more of the FAs on the 12.2 boulder',
-            imgLink: 'erichpurpur'
+            imgLink: 'erichpurpur',
+            author: 'Erich Purpur'
         },
         {
             id: 11,
-            title: 'Tommy Meriwether story',
+            title: 'Lockdown bushwhacking',
             active: true,
-            info: 'Tommy Meriwether story',
-            imgLink: 'tommymeriwether'
+            info: 'Peter Malander story',
+            imgLink: 'petermalander',
+            author: 'Peter Malander'
         },
         {
             id: 13,
             title: 'Tyler Hogan story',
             active: true,
             info: 'Tyler Hogan Story',
-            imgLink: 'tylerhogan'
+            imgLink: 'tylerhogan',
+            author: 'Tyler Hogan'
         },
         {
             id: 13,
             title: 'Mike Farnsworth story',
             active: true,
             info: 'Mike Farnsworth story',
-            imgLink: 'mikefarnsworth'
+            imgLink: 'mikefarnsworth',
+            author: 'Tyler Hogan'
         },
     ])
 
     return (
         <>
             <Header />
-            <h4 id="history">Choose a card to see a story in the history of greenstone bouldering</h4>
+            <h4 id="historyTitle">Choose a card to see a story in the history of greenstone bouldering</h4>
             <section id="historySection">
                 {/* map over history stories to display cards on screen */}
                 {historyStory.map((story) => 
                     <>
                         <Link to="/home" className="imgCard">
+                            <div className="textOverlay"> {story.title} </div>
                             <img src={`/images/history_images/${story.imgLink}.png`} alt={`${story.title} screenshot`} className="imgCardImg"></img>
-                            <div className="overlay"> {story.title} </div>
                         </Link>
                     </>
                 )}
