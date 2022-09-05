fb7f22780da81e5bdfe9dd7b3f978c370b7c1914
Erich Purpur <epurpur@gmail.com>
2022-09-05 13:08:17 -0400

    adding breadcrumb link

--- client/src/pages/EditorPage/index.js
+++ client/src/pages/EditorPage/index.js
@@ -1,7 +1,9 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 /* Components */
-
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 /* Styles */
 
@@ -19,11 +21,34 @@ const EditorPage = () => {
 
     return (
         <>
+            <Header />
+            <div id='editorFormHolder'>
+                {/* FIX BREADCRUMB HERE!!! START HERE */}
+                <p id="breadcrumb">Go Back: <Link to='/home'>Home Page</Link></p> 
 
-            <p>Current Info</p>
-            {pageName === 'Area' && <>Create new area</>}
-            {pageName === 'Boulder' && <>create new boulder</>}
-            {pageName === 'Route' && <>Create new route</>}
+                <p>Current Info</p>
+
+                {
+                /* data needed to create new area: areaName, areaDescription, parkingDescription, approachDescription, latitude, longitude  */
+                pageName === 'Area' && <>Create new area</>
+                
+                }
+
+                {
+                /* data needed to create new boulder: boulderName, boulderDescription, areaID, latitude, longitude, boulderImgURL */
+                pageName === 'Boulder' && <>create new boulder</>
+                
+                }
+
+                {
+                /* data needed to create new route: routeName, routeDescription, firstAscent, routeGrade, routeQuality, boulderID, routeImgURL, routeYoutubeEmbedURL */
+                pageName === 'Route' && <>Create new route</>
+                
+                }
+
+            </div>
+
+            <Footer />
         </>
     )
 }
